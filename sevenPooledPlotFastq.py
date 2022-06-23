dfba9cae9f67fd9413a84a45cc60da169663c40c
Darlene Darlington Wagner <darlingtonwagner@gmail.com>
2022-06-23 17:32:37 -0400

    Add files via upload

--- sevenPooledPlotFastq.py
+++ sevenPooledPlotFastq.py
@@ -29,7 +29,7 @@ ch.setFormatter(formatter)
 #add ch to logger
 logger.addHandler(ch)
 
-parser = argparse.ArgumentParser(description='Computes sequence lengths and average PHRED for shuffled paired reads in fastq (Expects single fastq input)', usage="python sevenPooledPlotFastq.py filepath/filename1.fastq(.gz) filepath/filename2.fastq(.gz) filepath/filename3.fastq(.gz) filepath/filename4.fastq(.gz) filepath/filename5.fastq(.gz) filepath/filename6.fastq(.gz) filepath/filename7.fastq(.gz) --outputType [F/J/N]")
+parser = argparse.ArgumentParser(description='Computes sequence lengths and average PHRED for shuffled paired reads in fastq (Expects single fastq input)', usage="python sevenPooledPlotFastq.py filepath/filename1.fastq(.gz) filepath/filename2.fastq(.gz) filepath/filename3.fastq(.gz) filepath/filename4.fastq(.gz) filepath/filename5.fastq(.gz) filepath/filename6.fastq(.gz) filepath/filename7.fastq(.gz) --outputType [F/C/N]")
 
 parser.add_argument('filename1', type=ext_check('.fastq', 'fastq.gz', argparse.FileType('r')))
 parser.add_argument('filename2', type=ext_check('.fastq', 'fastq.gz', argparse.FileType('r')))
@@ -39,7 +39,7 @@ parser.add_argument('filename5', type=ext_check('.fastq', 'fastq.gz', argparse.F
 parser.add_argument('filename6', type=ext_check('.fastq', 'fastq.gz', argparse.FileType('r')))
 parser.add_argument('filename7', type=ext_check('.fastq', 'fastq.gz', argparse.FileType('r')))
 
-parser.add_argument('--outputType', '-o', default='F', choices=['F', 'J', 'N'], help="--outputType F for full output,... J for .json only, and N for no image.")
+parser.add_argument('--outputType', '-o', default='F', choices=['F', 'C', 'N'], help="--outputType F for full output including plot of total PHRED scores, C for pooled output and individual summary files with no plot, and N for pooled output only, no image.")
 
 parser.add_argument('--title', '-t', default='Norovirus', help="Title for plot and output file names, if applicable")
 
@@ -383,14 +383,14 @@ print(avgLenFwd)
 #lisMinLenFwd = set1[fastqName1+'_R1_Len'].to_numpy(np.float64) 
 minLenFwd = min(lisAvgLenFwd)
 print(minLenFwd)
-lisMedFwdAvgPHRED = set1[fastqName1+'_R1_Qual'].to_numpy(np.float64).tolist() + set2[fastqName2+'_R1_Qual'].to_numpy(np.float64).tolist() + set3[fastqName3+'_R1_Qual'].to_numpy(np.float64).tolist() + set4[fastqName4+"_R1_Qual"].to_numpy(np.float64).tolist() + set5[fastqName5+"_R1_Qual"].to_numpy(np.float64).tolist() + set6[fastqName6+"_R1_Qual"].to_numpy(np.float64).tolist() + set7[fastqName7+"_R1_Qual"].to_numpy(np.float64).tolist()
-medFwdAvgPHRED = np.percentile(lisMedFwdAvgPHRED, 50)
+lisFwdAvgPHRED = set1[fastqName1+'_R1_Qual'].to_numpy(np.float64).tolist() + set2[fastqName2+'_R1_Qual'].to_numpy(np.float64).tolist() + set3[fastqName3+'_R1_Qual'].to_numpy(np.float64).tolist() + set4[fastqName4+"_R1_Qual"].to_numpy(np.float64).tolist() + set5[fastqName5+"_R1_Qual"].to_numpy(np.float64).tolist() + set6[fastqName6+"_R1_Qual"].to_numpy(np.float64).tolist() + set7[fastqName7+"_R1_Qual"].to_numpy(np.float64).tolist()
+medFwdAvgPHRED = np.percentile(lisFwdAvgPHRED, 50)
 print(medFwdAvgPHRED)
 #lqFwdAvgPHRED = np.percentile(set1[fastqName1+'_R1_Qual', fastqName2+'_R1_Qual'].values.tolist(), 25)
 #uqFwdAvgPHRED = np.percentile(set1[fastqName1+'_R1_Qual', fastqName2+'_R1_Qual'].values.tolist(), 75)
-avgFwdAvgPHRED = np.mean(lisMedFwdAvgPHRED)
+avgFwdAvgPHRED = np.mean(lisFwdAvgPHRED)
 print(avgFwdAvgPHRED)
-minFwdAvgPHRED = min(lisMedFwdAvgPHRED)
+minFwdAvgPHRED = min(lisFwdAvgPHRED)
 print(minFwdAvgPHRED)
 
 lisMedLenRev = set1[fastqName1+'_R2_Len'].to_numpy(np.float64).tolist() + set2[fastqName2+'_R2_Len'].to_numpy(np.float64).tolist() + set3[fastqName3+'_R2_Len'].to_numpy(np.float64).tolist() + set4[fastqName4+"_R2_Len"].to_numpy(np.float64).tolist() + set5[fastqName5+"_R2_Len"].to_numpy(np.float64).tolist() + set6[fastqName6+"_R2_Len"].to_numpy(np.float64).tolist() + set7[fastqName7+"_R2_Len"].to_numpy(np.float64).tolist()
@@ -400,14 +400,14 @@ avgLenRev = np.mean(lisMedLenRev)
 print(avgLenRev)
 minLenRev = min(lisMedLenRev)
 print(minLenRev)
-lisMedRevPHRED = set1[fastqName1+'_R2_Qual'].to_numpy(np.float64).tolist() + set2[fastqName2+'_R2_Qual'].to_numpy(np.float64).tolist() + set3[fastqName3+'_R2_Qual'].to_numpy(np.float64).tolist() + set4[fastqName4+"_R2_Qual"].to_numpy(np.float64).tolist() + set5[fastqName5+"_R2_Qual"].to_numpy(np.float64).tolist() + set6[fastqName6+"_R2_Qual"].to_numpy(np.float64).tolist() + set7[fastqName7+"_R2_Qual"].to_numpy(np.float64).tolist()
-medRevAvgPHRED = np.percentile(lisMedRevPHRED, 50)
+lisRevAvgPHRED = set1[fastqName1+'_R2_Qual'].to_numpy(np.float64).tolist() + set2[fastqName2+'_R2_Qual'].to_numpy(np.float64).tolist() + set3[fastqName3+'_R2_Qual'].to_numpy(np.float64).tolist() + set4[fastqName4+"_R2_Qual"].to_numpy(np.float64).tolist() + set5[fastqName5+"_R2_Qual"].to_numpy(np.float64).tolist() + set6[fastqName6+"_R2_Qual"].to_numpy(np.float64).tolist() + set7[fastqName7+"_R2_Qual"].to_numpy(np.float64).tolist()
+medRevAvgPHRED = np.percentile(lisRevAvgPHRED, 50)
 print(medRevAvgPHRED)
 #lqRevAvgPHRED = np.percentile(set1[fastqName1+'_R2_Qual', fastqName2+'_R2_Qual',].values.tolist(), 25)
 #uqRevAvgPHRED = np.percentile(set1[fastqName1+'_R2_Qual', fastqName2+'_R2_Qual',].values.tolist(), 75)
-avgRevAvgPHRED = np.mean(lisMedRevPHRED)
+avgRevAvgPHRED = np.mean(lisRevAvgPHRED)
 print(avgRevAvgPHRED)
-minRevAvgPHRED = min(lisMedRevPHRED)
+minRevAvgPHRED = min(lisRevAvgPHRED)
 print(minRevAvgPHRED)
 
 #forwardAvg = np.random.normal(size = 500000) + 30
@@ -423,42 +423,94 @@ else:
 fig.text(0.04, 0.5, 'Read Counts', va='center', rotation='vertical')
 
 ## Plot PHRED Quality for R1 reads as 1D histogram
-axes[0].hist(lisMedFwdAvgPHRED, bins = 25, color='blue')
+axes[0].hist(lisFwdAvgPHRED, bins = 25, color='blue')
 axes[0].set_title("R1 Quality " + newTitle)
 
 ## Plot PHRED Quality for R2 reads as 1D histogram
-axes[1].hist(lisMedRevPHRED, bins = 25, color='red')
+axes[1].hist(lisRevAvgPHRED, bins = 25, color='red')
 axes[1].set_title("R2 Quality " + newTitle)
 
 ## create output folder
-if(args.outputType != 'J'):
-        os.mkdir('QC_' + newTitle)
+os.mkdir('QC_' + newTitle)
 
 ## write figure file as .png
 if(args.outputType == 'F'):
-        fig.savefig('/scicomp/home-pure/ydn3/test_Python3.9.1/test_Biopython/QC_' + newTitle + '/fwd_and_revPHRED_histo.png')
+        fig.savefig('/scicomp/home-pure/ydn3/test_Python3.9.1/test_Biopython/QC_' + newTitle + '/fwd_and_revPHRED_allSevenHisto.png')
+
+        totalPHRED = { "R1_Reads_Avg_PHRED" : lisFwdAvgPHRED, "R2_Reads_Avg_PHRED" : lisRevAvgPHRED }
+        dfTotalPHRED = pd.DataFrame(totalPHRED)
+        dfTotalPHRED.to_csv('/scicomp/home-pure/ydn3/test_Python3.9.1/test_Biopython/QC_' + newTitle + '/fwd_and_revPHRED_allSeven.csv')
 
 ## write summary statistics into human readable text file
-if(args.outputType != 'J'):
-        summary = open('/scicomp/home-pure/ydn3/test_Python3.9.1/test_Biopython/QC_' + newTitle + '/ReadStatistics.README.txt', 'w')
+if(args.outputType != 'N'):
+        summary = open('/scicomp/home-pure/ydn3/test_Python3.9.1/test_Biopython/QC_' + newTitle + '/AllSevenReadStatistics.README.txt', 'w')
         summary.write("Summary Statistics for " + newTitle + "\n")
         summary.write("%s\t%0.2f" % ("R1 median length:", medLenFwd) + ",\t%s\t%0.2f\n" % ("R2 median length:", medLenRev))
         summary.write("%s\t%0.2f" % ("R1 mean length:", avgLenFwd) + ",\t%s\t%0.2f\n" % ("R1 mean length:", avgLenRev))
         summary.write("%s\t%s" % ("R1 minimum length:", str(minLenFwd)) + ",\t%s\t%s\n" % ("R2 minimum length:", str(minLenRev)))
         summary.write("%s\t%0.2f" % ("R1 median PHRED:", medFwdAvgPHRED) + ",\t%s\t%0.2f\n" % ("R2 median PHRED:", medRevAvgPHRED))
+        summary.write("%s\t%0.2f" % ("R1 mean PHRED:", avgFwdAvgPHRED)   + ",\t%s\t%0.2f\n" % ("R2 mean PHRED:", avgRevAvgPHRED))
         summary.write("%s\t%0.2f" % ("R1 minimum PHRED:", minFwdAvgPHRED) + ",\t%s\t%0.2f\n" % ("R2 minimum PHRED:", minRevAvgPHRED))
         summary.close()
 
+        summary1 = open('/scicomp/home-pure/ydn3/test_Python3.9.1/test_Biopython/QC_' + newTitle + '/' + fastqName1 + 'Statistics.README.txt', 'a')
+        summary1.write("Summary Statistics for " + fastqName1 + "\n")
+        set1.describe().to_string(summary1)
+        summary1.close()
+
+        summary2 = open('/scicomp/home-pure/ydn3/test_Python3.9.1/test_Biopython/QC_' + newTitle + '/' + fastqName2 + 'Statistics.README.txt', 'a')
+        summary2.write("Summary Statistics for " + fastqName2 + "\n")
+        set2.describe().to_string(summary2)
+        summary2.close()
+
+        summary3 = open('/scicomp/home-pure/ydn3/test_Python3.9.1/test_Biopython/QC_' + newTitle + '/' + fastqName3 + 'Statistics.README.txt', 'a')
+        summary3.write("Summary Statistics for " + fastqName3 + "\n")
+        set3.describe().to_string(summary3)
+        summary3.close()
+
+        summary4 = open('/scicomp/home-pure/ydn3/test_Python3.9.1/test_Biopython/QC_' + newTitle + '/' + fastqName4 + 'Statistics.README.txt', 'a')
+        summary4.write("Summary Statistics for " + fastqName4 + "\n")
+        set4.describe().to_string(summary4)
+        summary4.close()
+
+        summary5 = open('/scicomp/home-pure/ydn3/test_Python3.9.1/test_Biopython/QC_' + newTitle + '/' + fastqName5 + 'Statistics.README.txt', 'a')
+        summary5.write("Summary Statistics for " + fastqName5 + "\n")
+        set5.describe().to_string(summary5)
+        summary5.close()
+
+        summary6 = open('/scicomp/home-pure/ydn3/test_Python3.9.1/test_Biopython/QC_' + newTitle + '/' + fastqName6 + 'Statistics.README.txt', 'a')
+        summary6.write("Summary Statistics for " + fastqName6 + "\n")
+        set6.describe().to_string(summary6)
+        summary6.close()
+
+        summary7 = open('/scicomp/home-pure/ydn3/test_Python3.9.1/test_Biopython/QC_' + newTitle + '/' + fastqName7 + 'Statistics.README.txt', 'a')
+        summary7.write("Summary Statistics for " + fastqName7 + "\n")
+        set7.describe().to_string(summary7)
+        summary7.close()
+
+
+if(args.outputType == 'N'):
+        summary = open('/scicomp/home-pure/ydn3/test_Python3.9.1/test_Biopython/QC_' + newTitle + '/AllSevenReadStatistics.README.txt', 'w')
+        summary.write("Summary Statistics for " + newTitle + "\n")
+        summary.write("%s\t%0.2f" % ("R1 median length:", medLenFwd) + ",\t%s\t%0.2f\n" % ("R2 median length:", medLenRev))
+        summary.write("%s\t%0.2f" % ("R1 mean length:", avgLenFwd) + ",\t%s\t%0.2f\n" % ("R1 mean length:", avgLenRev))
+        summary.write("%s\t%s" % ("R1 minimum length:", str(minLenFwd)) + ",\t%s\t%s\n" % ("R2 minimum length:", str(minLenRev)))
+        summary.write("%s\t%0.2f" % ("R1 median PHRED:", medFwdAvgPHRED) + ",\t%s\t%0.2f\n" % ("R2 median PHRED:", medRevAvgPHRED))
+        summary.write("%s\t%0.2f" % ("R1 mean PHRED:", avgFwdAvgPHRED)   + ",\t%s\t%0.2f\n" % ("R2 mean PHRED:", avgRevAvgPHRED))
+        summary.write("%s\t%0.2f" % ("R1 minimum PHRED:", minFwdAvgPHRED) + ",\t%s\t%0.2f\n" % ("R2 minimum PHRED:", minRevAvgPHRED))
+        summary.close()
+
+
 ## write summary statistics into json file
 
-jsonDict = {
-        newTitle : {"read length" : { "median" : { "R1" : medLenFwd, "R2" : medLenRev }, "average" : { "R1" : avgLenFwd, "R2" : avgLenRev }, "minimum" : { "R1" : minLenFwd, "R2" : minLenRev } }, 
-                    "read PHRED quality" : { "median" : { "R1" : medFwdAvgPHRED, "R2" : medRevAvgPHRED }, "average" : {"R1" : avgFwdAvgPHRED, "R2" : avgRevAvgPHRED}, "minimum" : {"R1" : minFwdAvgPHRED, "R2" : minRevAvgPHRED} }, "Read Pairing Preserved" : pairingPreserved } }
+#jsonDict = {
+#        newTitle : {"read length" : { "median" : { "R1" : medLenFwd, "R2" : medLenRev }, "average" : { "R1" : avgLenFwd, "R2" : avgLenRev }, "minimum" : { "R1" : minLenFwd, "R2" : minLenRev } }, 
+#                    "read PHRED quality" : { "median" : { "R1" : medFwdAvgPHRED, "R2" : medRevAvgPHRED }, "average" : {"R1" : avgFwdAvgPHRED, "R2" : avgRevAvgPHRED}, "minimum" : {"R1" : minFwdAvgPHRED, "R2" : minRevAvgPHRED} }, "Read Pairing Preserved" : pairingPreserved } }
 
-if(args.outputType == 'J'):
-        with open(basePath + "/" + inPath1[0] + "/QC_" + newTitle + ".json", "w") as jsummary:
-                json.dump(jsonDict, jsummary)
-else:
-        with open("/scicomp/home-pure/ydn3/test_Python3.9.1/test_Biopython/QC_" + newTitle + "/ReadStatistics.json", "w") as jsummary:
-                json.dump(jsonDict, jsummary)
+#if(args.outputType == 'J'):
+#        with open(basePath + "/" + inPath1[0] + "/QC_" + newTitle + ".json", "w") as jsummary:
+#                json.dump(jsonDict, jsummary)
+#else:
+#        with open("/scicomp/home-pure/ydn3/test_Python3.9.1/test_Biopython/QC_" + newTitle + "/ReadStatistics.json", "w") as jsummary:
+#                json.dump(jsonDict, jsummary)
 
