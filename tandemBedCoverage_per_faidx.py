3936958f1802db1f89cbcbaed4d787f8c097a3c2
darlenewagner <ydn3@cdc.gov>
2026-06-15 17:25:49 -0400

    for two plots.

--- tandemBedCoverage_per_faidx.py
+++ tandemBedCoverage_per_faidx.py
@@ -26,11 +26,13 @@ def ext_check2(fai_ext, other_fai_ext, openner):
         return extension
 
 
-parser = argparse.ArgumentParser(description='Plots coverage from a .bed file and its .faidx file as a line plot', usage="plotBedCoverage.py filepath/filename.bed filepath/filename.fasta.faidx")
+parser = argparse.ArgumentParser(description='Plots coverage from a .bed file and its .faidx file as a line plot', usage="plotBedCoverage.py filepath/filename1.bed filepath/filename2.bed filepath/filename.fasta.faidx")
 
 parser.add_argument('filename1', type=ext_check1('.bedGraph', '.bed', argparse.FileType('r')))
 
-parser.add_argument('filename2', type=ext_check2('.fasta.fai', '.fa.fai', argparse.FileType('r')))
+parser.add_argument('filename2', type=ext_check1('.bedGraph', '.bed', argparse.FileType('r')))
+
+parser.add_argument('filename3', type=ext_check2('.fasta.fai', '.fa.fai', argparse.FileType('r')))
 
 parser.add_argument('--window', '-w', default='10', type=int)
 
@@ -68,9 +70,10 @@ elif window > 14:
 print("window = " + str(window))
 
 first_line = "";
-myCoverage = open(args.filename1.name, "r")
+myCoverage1 = open(args.filename1.name, "r")
+myCoverage2 = open(args.filename2.name, "r")
 
-with open(args.filename2.name, "r") as myLength:
+with open(args.filename3.name, "r") as myLength:
         first_line = myLength.readline().rstrip('\n')
 
 genomeLength = re.split(r'\s+', first_line)
@@ -79,7 +82,7 @@ myInitialCoord = 1
 myEndCoord = int(genomeLength[1])
 print(myEndCoord)
 
-for line in myCoverage:
+for line in myCoverage1:
         lineData = re.split(r'\s+', line)
         referenceName = lineData[0]
         coordinates.append(int(lineData[1]))
@@ -87,56 +90,108 @@ for line in myCoverage:
         iter = iter + 1
 
 idx = 0
-adjustedCoordinate = []
-hiAdjustedCoordinates = []
-hiAdjustedCoverage = []
-loAdjustedCoordinates = []
-loAdjustedCoverage = []
+adjustedCoordinate1 = []
+hiAdjustedCoordinates1 = []
+hiAdjustedCoverage1 = []
+loAdjustedCoordinates1 = []
+loAdjustedCoverage1 = []
 
 
 while myInitialCoord < myEndCoord - 1:
         if(( myInitialCoord == coordinates[idx] ) and (idx < len(coordinates) - 1) ):
                 if(coverage[idx] < 21):
-                        loAdjustedCoverage.append(coverage[idx])
-                        loAdjustedCoordinates.append( myInitialCoord )
-                        adjustedCoordinate.append( myInitialCoord )
+                        loAdjustedCoverage1.append(coverage[idx])
+                        loAdjustedCoordinates1.append( myInitialCoord )
+                        adjustedCoordinate1.append( myInitialCoord )
                 else:
-                        hiAdjustedCoverage.append(coverage[idx])
-                        hiAdjustedCoordinates.append( myInitialCoord )
-                        adjustedCoordinate.append( myInitialCoord )
+                        hiAdjustedCoverage1.append(coverage[idx])
+                        hiAdjustedCoordinates1.append( myInitialCoord )
+                        adjustedCoordinate1.append( myInitialCoord )
                 idx = idx + 1
                 myInitialCoord = myInitialCoord + 1
         else:
                 #hiAdjustedCoverage.append( 0 )
                 #hiAdjustedCoordinates.append( myInitialCoord )
-                loAdjustedCoverage.append( 0 )
-                loAdjustedCoordinates.append( myInitialCoord )
-                adjustedCoordinate.append( myInitialCoord )
+                loAdjustedCoverage1.append( 0 )
+                loAdjustedCoordinates1.append( myInitialCoord )
+                adjustedCoordinate1.append( myInitialCoord )
                 myInitialCoord = myInitialCoord + 1
 
-for t in loAdjustedCoverage:
-     print(t)
+iter = 0
+
+coordinates = []
+coverage = []
+
+for line in myCoverage2:
+        lineData = re.split(r'\s+', line)
+        referenceName = lineData[0]
+        coordinates.append(int(lineData[1]))
+        coverage.append(int(lineData[2]))
+        iter = iter + 1
+
+idx = 0
+adjustedCoordinate2 = []
+hiAdjustedCoordinates2 = []
+hiAdjustedCoverage2 = []
+loAdjustedCoordinates2 = []
+loAdjustedCoverage2 = []
+
+
+while myInitialCoord < myEndCoord - 1:
+        if(( myInitialCoord == coordinates[idx] ) and (idx < len(coordinates) - 1) ):
+                if(coverage[idx] < 21):
+                        loAdjustedCoverage2.append(coverage[idx])
+                        loAdjustedCoordinates2.append( myInitialCoord )
+                        adjustedCoordinate2.append( myInitialCoord )
+                else:
+                        hiAdjustedCoverage2.append(coverage[idx])
+                        hiAdjustedCoordinates2.append( myInitialCoord )
+                        adjustedCoordinate2.append( myInitialCoord )
+                idx = idx + 1
+                myInitialCoord = myInitialCoord + 1
+        else:
+                #hiAdjustedCoverage.append( 0 )
+                #hiAdjustedCoordinates.append( myInitialCoord )
+                loAdjustedCoverage2.append( 0 )
+                loAdjustedCoordinates2.append( myInitialCoord )
+                adjustedCoordinate2.append( myInitialCoord )
+                myInitialCoord = myInitialCoord + 1
+
+
+
+
+  #for t in loAdjustedCoverage1:
+  #     print(t)
 
 myXticks = []
 iter = 0
 # Generate X-axis labels
-for count in adjustedCoordinate:
+for count in adjustedCoordinate1:
         if(iter % 500 == 0):
                 myXticks.append(count)
         iter = iter + 1
 
 
 ## A single plot in the subplots.  Padding of 15% on bottom margin and 10% for the other three margins.
-fig, axes = plt.subplots(nrows=1, ncols=1, sharex=True, sharey=True, figsize=(10,7), gridspec_kw=dict(left=0.1, right=0.9, bottom=0.15, top=0.9))
+fig, axes = plt.subplots(nrows=2, ncols=1, sharex=True, sharey=True, figsize=(10,7), gridspec_kw=dict(left=0.1, right=0.9, bottom=0.15, top=0.9))
 
-## Plot Genome Coverage as line plot
-axes.plot(hiAdjustedCoordinates, hiAdjustedCoverage, linestyle='None', marker='.')
-axes.plot(loAdjustedCoordinates, loAdjustedCoverage, linestyle='None', color='red', marker='.')
-axes.set_xticks(myXticks, myXticks, rotation='vertical')
-axes.set_title("Sample " + shortTitle + " Reads")
-axes.set_xlabel('Reference Genome, ' + referenceName + ', Coordinates')
-axes.set_ylabel('Coverage (X) at Position')
+## Plot 1 Genome Coverage as line plot
+axes[0].plot(hiAdjustedCoordinates1, hiAdjustedCoverage1, linestyle='None', marker='.')
+axes[0].plot(loAdjustedCoordinates1, loAdjustedCoverage1, linestyle='None', color='red', marker='.')
+axes[0].set_xticks(myXticks, myXticks, rotation='vertical')
+axes[0].set_title("Sample " + shortTitle + " Reads")
+axes[0].set_xlabel('Reference Genome, ' + referenceName + ', Coordinates')
+axes[0].set_ylabel('Coverage (X) at Position')
 #axes.margins(0.2)
 
-fig.savefig('/scicomp/home-pure/ydn3/NGS_Plot_Widgets/Hidden_Files/win' + str(window) + '_' + shortTitle + '_to_' + referenceName + '.png')
+## Plot 2 Genome Coverage as line plot
+axes[1].plot(hiAdjustedCoordinates2, hiAdjustedCoverage2, linestyle='None', marker='.')
+axes[1].plot(loAdjustedCoordinates2, loAdjustedCoverage2, linestyle='None', color='red', marker='.')
+axes[1].set_xticks(myXticks, myXticks, rotation='vertical')
+axes[1].set_title("Sample " + shortTitle + " Reads")
+axes[1].set_xlabel('Reference Genome, ' + referenceName + ', Coordinates')
+axes[1].set_ylabel('Coverage (X) at Position')
+
+
+fig.savefig('/scicomp/home-pure/ydn3/NGS_Plot_Widgets/Hidden_Files/tandem_win' + str(window) + '_' + shortTitle + '_to_' + referenceName + '.png')
 
